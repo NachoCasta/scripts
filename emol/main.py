f71c0ae4e493166ad077741254da7c14f24798eb
Nacho Castaneda <icastanedaw@gmail.com>
2018-12-02 23:25:49 -0300

    Comienzo obituario

--- emol/main.py
+++ emol/main.py
@@ -8,36 +8,42 @@ from mail import enviar_mail
 browser = RoboBrowser(history=True)
 browser.open('https://digital.elmercurio.com')
 
-paginas = browser.find_all(class_="item")
-for pagina in paginas:
-    a = pagina.find("a")
-    onclick = a.get("onclick")
-    if onclick is None:
-        continue
-    args = [a.strip("'") for a in
-            onclick[onclick.index("(")+1:onclick.index(")")].split(",")]
-    letter, _, number = args
-    page = letter + number
-    if page == "A2":
-        browser.follow_link(a)
-        break
+def descargar_cartas():
+    paginas = browser.find_all(class_="item")
+    for pagina in paginas:
+        a = pagina.find("a")
+        onclick = a.get("onclick")
+        if onclick is None:
+            continue
+        args = [a.strip("'") for a in
+                onclick[onclick.index("(")+1:onclick.index(")")].split(",")]
+        letter, _, number = args
+        page = letter + number
+        if page == "A2":
+            browser.follow_link(a)
+            break
 
-imagen = browser.find(class_="fullImg").get("src-img")
-path = os.path.dirname(os.path.abspath(__file__)).replace("\\", "/")
-nombre = path + "/Cartas.jpg"
+    imagen = browser.find(class_="fullImg").get("src-img")
+    path = os.path.dirname(os.path.abspath(__file__)).replace("\\", "/")
+    nombre = path + "/Cartas.jpg"
 
 
-r = requests.get(imagen, stream=True,
-                 headers={'User-agent': 'Mozilla/5.0'})
-if r.status_code == 200:
-    with open(nombre, 'wb') as f:
-        r.raw.decode_content = True
-        shutil.copyfileobj(r.raw, f)
+    r = requests.get(imagen, stream=True,
+                     headers={'User-agent': 'Mozilla/5.0'})
+    if r.status_code == 200:
+        with open(nombre, 'wb') as f:
+            r.raw.decode_content = True
+            shutil.copyfileobj(r.raw, f)
 
-with open(path + "/mails.txt") as f:
-    lista = [m.strip() for m in f.readlines()]
+def get_mails(file):
+    with open(path + "/{}".format(file) as f:
+        lista = [m.strip() for m in f.readlines()]
+    return lista
 
-for mail in lista:
-    enviar_mail(mail,
-                strftime("Cartas al director del %d/%m/%Y"), "",
-                nombre)
+if __name__ == "__main__":
+    cartas = descargar_cartas()
+    mails_cartas = get_mails("mails_cartas")
+    for mail in mails:
+        enviar_mail(mail,
+                    strftime("Cartas al director del %d/%m/%Y"), "",
+                    nombre)
