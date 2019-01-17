b0ac2d278a1965bfc61150d211561bdd85b48213
Ignacio Castañeda <icastanedaw@gmail.com>
2019-01-17 18:50:29 -0300

    Traductor de preguntas

---
+++ translator/main.py
@@ -0,0 +1,15 @@
+from googletrans import Translator
+translator = Translator()
+
+with open("preguntas.txt", "r") as file:
+    preguntas = [l[3:].strip() for l in file]
+    print("Preguntas abiertas")
+
+traducidas = [translator.translate(p, src="de", dest="en").text
+              for p in preguntas]
+print("Preguntas traducidas")
+
+with open("traducidas.txt", "w") as file:
+    for i, (p, t) in enumerate(zip(preguntas, traducidas)):
+        file.write("{}. {}\n   ({})\n\n".format(i+1, p, t))
+    print("Archivo listo")
