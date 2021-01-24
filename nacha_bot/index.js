7ab33eaa37a8c3ebf9169773db3d5ced5260385b
Ignacio Castañeda <icastanedaw@gmail.com>
2021-01-24 14:44:34 -0300

    fix(config): rpi

--- nacha_bot/index.js
+++ nacha_bot/index.js
@@ -13,37 +13,41 @@ const DAYS_UNTIL_UNFOLLOW = 2;
 
 const LOGS = path.join(ROOT, "logs");
 
+const RASPBERRY_PI = process.env.RASPBERRY_PI;
+
 const options = {
     cookiesPath: path.join(LOGS, "./cookies.json"),
     username: process.env.USERNAME,
     password: process.env.PASSWORD,
     maxFollowsPerHour: 20,
-    maxFollowsPerDay: 150,
+    maxFollowsPerDay: 100,
     maxLikesPerDay: 50,
     dontUnfollowUntilTimeElapsed: DAYS_UNTIL_UNFOLLOW * 24 * 60 * 60 * 1000,
     // If true, will not do any actions (defaults to true)
-    dryRun: false,
+    dryRun: !RASPBERRY_PI,
 };
 
 (async () => {
     let browser;
 
     try {
-        browser = await puppeteer.launch({ headless: false });
+        const puppeteerOptions = RASPBERRY_PI
+            ? {
+                  executablePath: "/usr/bin/chromium-browser",
+                  headless: true,
+                  args: ["--disable-features=VizDisplayCompositor"],
+              }
+            : { headless: false };
+        browser = await puppeteer.launch(puppeteerOptions);
 
-        // Create a database where state will be loaded/saved to
         const instautoDb = await Instauto.JSONDB({
-            // Will store a list of all users that have been followed before, to prevent future re-following.
             followedDbPath: path.join(LOGS, "followed.json"),
-            // Will store all unfollowed users here
             unfollowedDbPath: path.join(LOGS, "unfollowed.json"),
-            // Will store all likes here
             likedPhotosDbPath: path.join(LOGS, "liked-photos.json"),
         });
 
         const instauto = await Instauto(instautoDb, browser, options);
 
-        // List of usernames that we should follow the followers of, can be celebrities etc.
         const usersToFollowFollowersOf = [
             "chelcy.cl",
             "cmoran_shoes",
@@ -58,7 +62,6 @@ const options = {
             "shoes_divino",
         ];
 
-        // Now go through each of these and follow a certain amount of their followers
         await instauto.followUsersFollowers({
             usersToFollowFollowersOf,
             skipPrivate: false,
@@ -66,9 +69,10 @@ const options = {
 
         await instauto.sleep(10 * 60 * 1000);
 
-        // Unfollow auto-followed users (regardless of whether they are following us)
-        // after a certain amount of days
-        await instauto.unfollowOldFollowed({ ageInDays: DAYS_UNTIL_UNFOLLOW });
+        await instauto.unfollowOldFollowed({
+            ageInDays: DAYS_UNTIL_UNFOLLOW,
+            limit: 150,
+        });
 
         console.log("Done running");
     } catch (err) {
--- nacha_bot/main.py
+++ nacha_bot/main.py
@@ -24,14 +24,9 @@ accounts = random.sample(["chelcy.cl", "cmoran_shoes", "luau_shoes", "chinitascl
 with smart_run(session):
     """ Activity flow """
     # general settings
-    session.set_quota_supervisor(enabled=True, peak_follows_daily=150, peak_follows_hourly=30, peak_unfollows_hourly=60
-                                 peak_unfollows_daily=200, sleep_after=["follows_h", "unfollows_h"], stochastic_flow=True, notify_me=True)
+    session.set_quota_supervisor(enabled=True, peak_follows_daily=150, peak_follows_hourly=30, peak_unfollows_hourly=60,
+                                 peak_unfollows_daily=300, sleep_after=["follows_h", "unfollows_h"], stochastic_flow=True, notify_me=True)
     session.set_skip_users(skip_private=False)
     # activity
     session.unfollow_users(amount=200, instapy_followed_enabled=True, instapy_followed_param="all",
                            style="RANDOM", unfollow_after=2*24*60*60, sleep_delay=5*60)
-    session.follow_user_followers(accounts, amount=10, randomize=False)
-    session.follow_likers(accounts, photos_grab_amount=2, follow_likers_per_photo=5,
-                          randomize=True, sleep_delay=600, interact=False)
-    session.like_by_feed(amount=100, randomize=True,
-                         unfollow=False, interact=False)
