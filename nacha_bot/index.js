cf424e96b8b3a42a014aa5670b3a62a1f57fae03
Ignacio Castañeda <icastanedaw@gmail.com>
2021-01-24 14:01:44 -0300

    feat(bot): add node bot

---
+++ nacha_bot/index.js
@@ -0,0 +1,81 @@
+"use strict";
+
+require("dotenv").config();
+
+const path = require("path");
+
+const puppeteer = require("puppeteer"); // eslint-disable-line import/no-extraneous-dependencies
+
+const Instauto = require("instauto"); // eslint-disable-line import/no-unresolved
+
+const DAYS_UNTIL_UNFOLLOW = 2;
+
+const ROOT = path.dirname(require.main.filename);
+
+const LOGS = path.join(ROOT, "logs");
+
+const options = {
+    cookiesPath: path.join(LOGS, "./cookies.json"),
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    maxFollowsPerHour: 20,
+    maxFollowsPerDay: 150,
+    maxLikesPerDay: 50,
+    dontUnfollowUntilTimeElapsed: DAYS_UNTIL_UNFOLLOW * 24 * 60 * 60 * 1000,
+    // If true, will not do any actions (defaults to true)
+    dryRun: false,
+};
+
+(async () => {
+    let browser;
+
+    try {
+        browser = await puppeteer.launch({ headless: false });
+
+        // Create a database where state will be loaded/saved to
+        const instautoDb = await Instauto.JSONDB({
+            // Will store a list of all users that have been followed before, to prevent future re-following.
+            followedDbPath: path.join(LOGS, "followed.json"),
+            // Will store all unfollowed users here
+            unfollowedDbPath: path.join(LOGS, "unfollowed.json"),
+            // Will store all likes here
+            likedPhotosDbPath: path.join(LOGS, "liked-photos.json"),
+        });
+
+        const instauto = await Instauto(instautoDb, browser, options);
+
+        // List of usernames that we should follow the followers of, can be celebrities etc.
+        const usersToFollowFollowersOf = [
+            "chelcy.cl",
+            "cmoran_shoes",
+            "luau_shoes",
+            "chinitascl",
+            "zapatosmandalas",
+            "estelazapatos",
+            "fessiazapatos",
+            "grossa.cl",
+            "becca.shoesok",
+            "rameshoes.cl",
+            "shoes_divino",
+        ];
+
+        // Now go through each of these and follow a certain amount of their followers
+        await instauto.followUsersFollowers({
+            usersToFollowFollowersOf,
+            skipPrivate: false,
+        });
+
+        await instauto.sleep(10 * 60 * 1000);
+
+        // Unfollow auto-followed users (regardless of whether they are following us)
+        // after a certain amount of days
+        await instauto.unfollowOldFollowed({ ageInDays: DAYS_UNTIL_UNFOLLOW });
+
+        console.log("Done running");
+    } catch (err) {
+        console.error(err);
+    } finally {
+        console.log("Closing browser");
+        if (browser) await browser.close();
+    }
+})();
--- nacha_bot/main.py
+++ nacha_bot/main.py
@@ -1,5 +1,6 @@
 # imports
 import os
+import random
 
 from instapy import InstaPy
 from instapy import smart_run
@@ -17,22 +18,20 @@ session = InstaPy(username=insta_username,
                   password=insta_password,
                   headless_browser=True)
 
-accounts = ["chelcy.cl", "cmoran_shoes", "luau_shoes", "chinitascl", "zapatosmandalas",
-            "estelazapatos", "fessiazapatos", "grossa.cl", "becca.shoesok", "rameshoes.cl", "shoes_divino"]
+accounts = random.sample(["chelcy.cl", "cmoran_shoes", "luau_shoes", "chinitascl", "zapatosmandalas",
+                          "estelazapatos", "fessiazapatos", "grossa.cl", "becca.shoesok", "rameshoes.cl", "shoes_divino"], 10)
 
 with smart_run(session):
     """ Activity flow """
     # general settings
-    session.set_quota_supervisor(enabled=True, peak_follows_daily=150, peak_follows_hourly=30, peak_unfollows_hourly=40,
-                                 peak_unfollows_daily=200, sleep_after=["follows_h", "unfollows_d"], stochastic_flow=True, notify_me=True)
+    session.set_quota_supervisor(enabled=True, peak_follows_daily=150, peak_follows_hourly=30, peak_unfollows_hourly=60
+                                 peak_unfollows_daily=200, sleep_after=["follows_h", "unfollows_h"], stochastic_flow=True, notify_me=True)
     session.set_skip_users(skip_private=False)
     # activity
-    session.unfollow_users(amount=100, instapy_followed_enabled=True, instapy_followed_param="all",
+    session.unfollow_users(amount=200, instapy_followed_enabled=True, instapy_followed_param="all",
                            style="RANDOM", unfollow_after=2*24*60*60, sleep_delay=5*60)
     session.follow_user_followers(accounts, amount=10, randomize=False)
-    session.unfollow_users(amount=100, instapy_followed_enabled=True, instapy_followed_param="all",
-                           style="RANDOM", unfollow_after=2*24*60*60, sleep_delay=5*60)
-    session.follow_likers(accounts, photos_grab_amount=2, follow_likers_per_photo=10,
+    session.follow_likers(accounts, photos_grab_amount=2, follow_likers_per_photo=5,
                           randomize=True, sleep_delay=600, interact=False)
     session.like_by_feed(amount=100, randomize=True,
                          unfollow=False, interact=False)
