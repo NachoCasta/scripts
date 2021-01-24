cbd2e8a8fe94e5d132816e8fbd0a390a928fc361
Ignacio Castañeda <icastanedaw@gmail.com>
2021-01-24 15:51:02 -0300

    feat(config): modify params

--- nacha_bot/index.js
+++ nacha_bot/index.js
@@ -9,22 +9,29 @@ const puppeteer = require("puppeteer"); // eslint-disable-line import/no-extrane
 
 const Instauto = require("instauto"); // eslint-disable-line import/no-unresolved
 
-const DAYS_UNTIL_UNFOLLOW = 2;
-
 const LOGS = path.join(ROOT, "logs");
 
-const RASPBERRY_PI = process.env.RASPBERRY_PI;
+const RASPBERRY_PI = process.env.RASPBERRY_PI === "true";
+
+const DAYS_UNTIL_UNFOLLOW = 2;
+const FOLLOWS_PER_HOUR = 20;
+const RANDOM_FOLLOWS_PER_HOUR = Math.floor(
+    Math.random() * 2 + FOLLOWS_PER_HOUR
+);
+const FOLLOWS_PER_DAY = 100;
+const RANDOM_FOLLOWS_PER_DAY = Math.floor(Math.random() * 10 + FOLLOWS_PER_DAY);
 
 const options = {
     cookiesPath: path.join(LOGS, "./cookies.json"),
     username: process.env.USERNAME,
     password: process.env.PASSWORD,
-    maxFollowsPerHour: 20,
-    maxFollowsPerDay: 100,
-    maxLikesPerDay: 50,
+    maxFollowsPerHour: RANDOM_FOLLOWS_PER_HOUR,
+    maxFollowsPerDay: RANDOM_FOLLOWS_PER_DAY,
+    followUserRatioMin: null,
+    followUserRatioMax: null,
     dontUnfollowUntilTimeElapsed: DAYS_UNTIL_UNFOLLOW * 24 * 60 * 60 * 1000,
     // If true, will not do any actions (defaults to true)
-    dryRun: !RASPBERRY_PI,
+    dryRun: false,
 };
 
 (async () => {
@@ -65,13 +72,14 @@ const options = {
         await instauto.followUsersFollowers({
             usersToFollowFollowersOf,
             skipPrivate: false,
+            maxFollowsTotal: RANDOM_FOLLOWS_PER_DAY,
         });
 
         await instauto.sleep(10 * 60 * 1000);
 
         await instauto.unfollowOldFollowed({
             ageInDays: DAYS_UNTIL_UNFOLLOW,
-            limit: 150,
+            limit: FOLLOWS_PER_DAY + 50,
         });
 
         console.log("Done running");
